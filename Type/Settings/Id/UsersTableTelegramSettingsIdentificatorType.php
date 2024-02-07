488b0526e19f634b0952b795b1eda81c2dd09590
kingstar <admin@baks.dev>
2024-02-08 00:12:57 +0300

    Baks Development

--- Type/Settings/Id/UsersTableTelegramSettingsIdentificatorType.php
+++ Type/Settings/Id/UsersTableTelegramSettingsIdentificatorType.php
@@ -20,17 +20,18 @@ namespace BaksDev\Telegram\Bot\Type\Settings\Id;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
 
-final class UsersTableTelegramSettingsIdentificatorType extends StringType
+final class UsersTableTelegramSettingsIdentificatorType extends Type
 {
-    public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
     {
         return (string) $value;
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform): mixed
+    public function convertToPHPValue($value, AbstractPlatform $platform): UsersTableTelegramSettingsIdentificator
     {
-        return !empty($value) ? new UsersTableTelegramSettingsIdentificator() : $value;
+        return  new UsersTableTelegramSettingsIdentificator();
     }
 
     public function getName(): string
@@ -41,5 +42,10 @@ final class UsersTableTelegramSettingsIdentificatorType extends StringType
     public function requiresSQLCommentHint(AbstractPlatform $platform): bool
     {
         return true;
+    }
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getStringTypeDeclarationSQL($column);
     }
 }
