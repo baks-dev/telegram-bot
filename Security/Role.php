6d145986957651348c322b0ea993695e73b1ae05
MichelAngelo <baksdevelopmen@gmail.com>
2025-05-22 07:25:30 +0300

    BaksDevelopment

--- Security/Role.php
+++ Security/Role.php
@@ -35,7 +35,9 @@ use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 #[AutoconfigureTag('baks.menu.admin')]
 final class Role implements RoleInterface, MenuAdminInterface
 {
-    public const ROLE = 'ROLE_TELEGRAM_SETTINGS';
+    public const string ROLE = 'ROLE_TELEGRAM_SETTINGS';
+
+    public const string KEY = 'XGDESEDjh';
 
     public function getRole(): string
     {
@@ -50,6 +52,14 @@ final class Role implements RoleInterface, MenuAdminInterface
     public function getPath(): string
     {
         return 'telegram-bot:admin.settings';
+    }
+
+    /**
+     * Метод возвращает ключ раздела (для меню телеграм)
+     */
+    public function getPathKey(): string
+    {
+        return self::KEY;
     }
 
     /** Метод возвращает секцию, в которую помещается ссылка на раздел */
